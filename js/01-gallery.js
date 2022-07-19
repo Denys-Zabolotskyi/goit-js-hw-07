762c902351061ed160e3596363253be16935551f
Denys-Zabolotskyi <d.zabolotskyi@gmail.com>
2022-07-19 23:45:15 +0300

    Сompleted task-01

---
+++ js/01-gallery.js
@@ -0,0 +1,92 @@
+//*****************************************Markup************************************************ */
+{
+  /* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+}
+//*****************************************************************************************
+// console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+//*****************************************************************************************
+// Change code below this line
+//*********************************Ref on element******************************************
+const galleryEl = document.querySelector('.gallery');
+//*****************************************************************************************
+
+//**********************************FunctionCreateMarkup*******************************************************
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      // console.log(item);
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+const createGallery = createGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', createGallery);
+
+//**********************************addEventListener*******************************************************
+
+galleryEl.addEventListener('click', onGalleryClickImg);
+
+function onGalleryClickImg(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+  document.addEventListener('keydown', closeEscape);
+  function closeEscape(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', closeEscape);
+    }
+  }
+}
+
+//**************************************Another code***************************************************
+//*** #1 *** */
+// const createGalleryItemsMarkup = items => {
+//   return `
+// <div class="gallery__item">
+//   <a class="gallery__link" href="${items.original}">
+//     <img
+//       class="gallery__image"
+//       src="${items.preview}"
+//       data-source="${items.original}"
+//       alt="${items.description}"
+//     />
+//   </a>
+// </div>
+//     `;
+// };
+// // console.log(createGalleryItemsMarkup(galleryItems[0]));
+//*** #2 *** */
+// const createGallery = galleryItems.map(createGalleryItemsMarkup).join('');
+// console.log(createGallery);
+//*** #3 *** */
+// galleryEl.insertAdjacentHTML('beforeend', createGallery);
+//*****************************************************************************************
