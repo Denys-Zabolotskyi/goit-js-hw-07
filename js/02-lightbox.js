e3d562c18d7fd71b3e65624dd739066c1baaa73f
Denys-Zabolotskyi <d.zabolotskyi@gmail.com>
2022-07-20 00:35:44 +0300

    Сompleted task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+//*********************************Ref on element******************************************
+const galleryEl = document.querySelector('.gallery');
+//*****************************************************************************************
+//**********************************FunctionCreateMarkup*******************************************************
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join('');
+}
+const createGallery = createGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', createGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  //   captionPosition: 'top',
+  //   captionPosition: 'outside',
+  /* options */
+});
